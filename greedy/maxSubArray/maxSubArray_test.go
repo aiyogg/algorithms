efce2237444449dd23fc03a7617bd01de4f7264b
chuck <tengchenc@gmail.com>
2020-01-05 16:35:09 +0800

    add maxSubArray

---
+++ greedy/maxSubArray/maxSubArray_test.go
@@ -0,0 +1,27 @@
+package maxSubArray
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var tcs = []struct {
+	nums []int
+	ans  int
+}{
+	{
+		[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+		6,
+	},
+	{
+		[]int{-2},
+		-2,
+	},
+}
+
+func TestMaxSubArray(t *testing.T) {
+	ast := assert.New(t)
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, maxSubArray(tc.nums))
+	}
+}
